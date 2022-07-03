4ba571312334f155d209e8d70d1fc5cae043c035
Matteo Verzeroli <matteoverzeroli@live.it>
2022-07-03 17:42:06 +0200

    added support for 2 lines button layout.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import glob, os
 
 if len(glob.glob("*.xml")) == 1:
     with open(glob.glob("*.xml").pop(), 'r') as f:
-      data = f.read()
+        data = f.read()
 elif len(glob.glob("*.xml")) > 1:
     print("Più di un file xml trovato!")
     exit(1)
@@ -25,28 +25,35 @@ if __name__ == "__main__":
     playback_pg_number = "1"
 
     bs_data = BeautifulSoup(data, 'xml')
-    cuelists_type = ('Cuelist','Chase', 'Override','Submaster','Timecode','Groupmaster')
+    cuelists_type = ('Cuelist', 'Chase', 'Override', 'Submaster', 'Timecode', 'Groupmaster')
     for type in cuelists_type:
-      cuelists = bs_data.find_all(type)
-      for cuelist in cuelists:
-          cl_btn = cuelist.find('PlayBackButton')
-          if cl_btn is not None:
-              cl_name = cuelist.get('cuelistName')
-              cl_playback_pg = cl_btn.get('playbackPage')
-              cl_playback_pos = cl_btn.get('buttonPosition')
-              if cl_playback_pg == playback_pg_number:
-                  playback[cl_playback_pos] = cl_name
+        cuelists = bs_data.find_all(type)
+        for cuelist in cuelists:
+            cl_btn = cuelist.find('PlayBackButton')
+            if cl_btn is not None:
+                cl_name = cuelist.get('cuelistName')
+                cl_playback_pg = cl_btn.get('playbackPage')
+                cl_playback_pos = cl_btn.get('buttonPosition')
+                if cl_playback_pg == playback_pg_number:
+                    playback[cl_playback_pos] = cl_name
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", default= IP,
+    parser.add_argument("--ip", default=IP,
                         help="The ip of the OSC server")
-    parser.add_argument("--port", type=int, default= int(PORT),
+    parser.add_argument("--port", type=int, default=int(PORT),
                         help="The port the OSC server is listening on")
     args = parser.parse_args()
 
     client = udp_client.SimpleUDPClient(args.ip, args.port)
 
     for btn in playback:
-      print(btn +": " + playback[btn])
-      client.send_message("/PB/" + btn, playback[btn])
+        print(btn + ": " + playback[btn])
+        temp = playback[btn].split()
+        if(len(temp)) > 1:
+            client.send_message("/PB/" + btn + "/2", temp[-1])
+            temp.pop()
+            client.send_message("/PB/" + btn + "/1", " ".join(temp))
+        else:
+            client.send_message("/PB/" + btn + "/1", playback[btn])
+            client.send_message("/PB/" + btn + "/2", "")
     os.system("pause")
